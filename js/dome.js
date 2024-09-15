a432098208e2fd082617a42f1075d528cc3407e9
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-15 09:55:31 +0600

    id js

--- js/dome.js
+++ js/dome.js
@@ -1,2 +1,10 @@
 console.log('welecome js')
-console.log(document.body)
+// console.log(document.body)
+
+const fourtItem =document.getElementsByClassName('furat-item');
+for(let furte of fourtItem){
+    console.log(furte.innerText)
+}
+
+document.getElementById('fastTitle').style.backgroundColor = 'red'
+console.log(fourtItem)
