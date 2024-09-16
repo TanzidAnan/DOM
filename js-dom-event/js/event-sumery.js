20277f152a11c893c4031de2b190752fed379cbe
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-16 19:00:47 +0600

    click button click

---
+++ js-dom-event/js/event-sumery.js
@@ -0,0 +1,13 @@
+function hendleCard(){
+    console.log('button click');
+    const heandleChance =document.getElementById('heandle-adds');
+    heandleChance.innerText ='Tanzid anan marzan'
+    heandleChance.style.fontSize ='30px'
+    heandleChance.style.fontWeight =900
+}
+document.getElementById('add-event-liseaner').addEventListener('click',function(){
+const dataChace =document.getElementById('data-chance');
+const dataChace1 =document.getElementById('heandle-adds');
+dataChace.innerText ='ki data deaoa jai'
+dataChace1.innerText ='Tanzid anan marzan'
+})
