9309a2e391d2152672d6a100b65a8346ebe2191f
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-16 15:48:17 +0600

    funaction click

--- js-dom-event/js/event.js
+++ js-dom-event/js/event.js
@@ -3,3 +3,10 @@ console.log('this is seperated fild')
 function add(){
     document.body.style.backgroundColor = 'green'
 }
+
+const makeBlueButton =document.getElementById('make-blue');
+makeBlueButton.onclick =makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor ='blue';
+}
