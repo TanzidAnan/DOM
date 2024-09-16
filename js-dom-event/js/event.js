ce57251beeeb8e42bd6866029f788a2a629943d3
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-16 16:01:55 +0600

    button addEvent

--- js-dom-event/js/event.js
+++ js-dom-event/js/event.js
@@ -1,12 +1,19 @@
 console.log('this is seperated fild')
 
-function add(){
+function add() {
     document.body.style.backgroundColor = 'green'
 }
 
-const makeBlueButton =document.getElementById('make-blue');
-makeBlueButton.onclick =makeBlue;
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
 
-function makeBlue(){
-    document.body.style.backgroundColor ='blue';
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+const makeButton = document.getElementById('make-pink');
+makeButton.addEventListener('click', makepink)
+
+function makepink() {
+    document.body.style.backgroundColor = 'pink'
 }
