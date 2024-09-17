d8be774ba3c411a5da49772cd169cf3afa4996ea
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-17 23:35:20 +0600

    comment

--- add-event/reavveo.js
+++ add-event/reavveo.js
@@ -3,6 +3,9 @@ document.getElementById('submit-btn').addEventListener('click',function(){
     const inputFild = document.getElementById('text-area').value;
     const reviwInput =document.getElementById('reviw');
     const p =document.createElement('p');
+    if(p === undefined){
+        console.log(5656)
+    }
     p.innerText =setCount + " ."+ inputFild;
     reviwInput.appendChild(p);
     setCount ++;
