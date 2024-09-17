6229b46c2d4e7675849400dfbf4eeb240ecccf09
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-17 18:06:47 +0600

    count

--- add-event/secrip.js
+++ add-event/secrip.js
@@ -1,4 +1,16 @@
+let sum =0
 function clickMe(){
     const fastElement =document.getElementById('count');
-    console.log(fastElement)
+    sum += 1
+    if(sum>=0){
+        fastElement.innerText =sum
+    }
 }
+
+document.getElementById('odd-btn').addEventListener('click',function(){
+    const fastElement =document.getElementById('count');
+    sum-=1;
+    if(sum >=0){
+        fastElement.innerText =sum
+    }
+})
