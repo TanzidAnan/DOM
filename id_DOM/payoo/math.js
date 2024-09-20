94d5450fae330f48390049b7550c8b2afc050858
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-20 20:01:26 +0600

    funcation

---
+++ id_DOM/payoo/math.js
@@ -0,0 +1,4 @@
+function math(num){
+    const number =num*2;
+    return number
+}
---
+++ id_DOM/payoo/utlities.js
@@ -0,0 +1,4 @@
+function getIuputFildValueById(id){
+    const inputValue =document.getElementById(id).value;
+    return inputValue;
+}
