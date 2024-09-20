e5c98077ddf02b671edeee6cc667b29b8a212732
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-20 20:12:37 +0600

    funcation

--- id_DOM/payoo/addMony.js
+++ id_DOM/payoo/addMony.js
@@ -3,7 +3,14 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
 
     const addMoney =getIuputFildValueById('input-add-money');
     const inputPinNumber =getIuputFildValueById('input-pin-number');
-    console.log(addMoney,'Add money',inputPinNumber);
+    // console.log(addMoney,'Add money',inputPinNumber);
+
+    if(inputPinNumber === 1111){
+
+    }
+    else{
+        alert('Falid to pin and number')
+    }
     
 
 })
