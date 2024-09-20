2208f85631098877b3c0bf5ee666723ceecf5ada
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-20 20:04:10 +0600

    add mony

---
+++ id_DOM/payoo/addMony.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+
+    const addMoney =getIuputFildValueById('input-add-money');
+    console.log(addMoney);
+    
+
+})
